a5b8989a693aa50e35fb418acce3d6b49875e3bf
Ahmet Ozer <ozera@uci.edu>
2025-03-17 01:14:37 +0000

    m

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -46,10 +46,6 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& path, int total) {
-    if (path.empty()) {
-        cout << "No path found" << endl;
-        return;
-    }
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i] << " ";
     }
